7329bcfa04bbaf0369e536a38813d335f0e8da10
aaronzomback <aaronzomback@gmail.com>
2021-07-07 10:19:19 -0400

    added messages component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,15 +10,36 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+  
+  
+  // The parameter simultaneously defines a private heroService property 
+  // and identifies it as a HeroService injection site.
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  heroes = HEROES;
+  
+  // waits for the Observable to emit the array of heroes—which 
+  // could happen now or several minutes from now.
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+    // The subscribe() method passes the emitted array 
+    // to the callback, which sets the component's heroes property.
 
-
-  constructor() { }
-
-  ngOnInit(): void {
+    // This asynchronous approach will work 
+    // when the HeroService requests heroes from the server.
+  
   }
+
+  // When Angular creates a HeroesComponent, 
+  // the Dependency Injection system sets 
+  // the heroService parameter to the singleton instance of HeroService.
 
 }
